9bebb9080e4e0093be6e71afd72b0ae4f5042001
Taylor Abbott <68927611+taylornicole906@users.noreply.github.com>
2021-02-24 20:23:38 -0800

    Time: 28 ms (86.98%), Space: 14 MB (93.38%) - LeetHub

---
+++ house-robber/house-robber.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+            if len(nums) == 0:
+                return 0
+            if len(nums) == 1:
+                return nums[0]
+
+            # dp array stores max profit at each house.
+            # create first two elements in dp array
+            # first question, do we rob 1st house or second house?
+            dp = [nums[0], max(nums[0], nums[1])]
+
+            # if we rob 2nd house, next question is do we rob 1st + 3rd, or just second?
+            for i in range(2, len(nums)):
+                dp.append(max(dp[i - 1], dp[i - 2] + nums[i]))
+
+            return max(dp)
+            
+            
+            
+                
+        

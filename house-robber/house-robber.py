0b3fa690a86f30219e7ddd9de3a3fa7e4ec865aa
Taylor Abbott <68927611+taylornicole906@users.noreply.github.com>
2021-02-25 20:16:43 -0800

    Time: 32 ms (66.00%), Space: 14.1 MB (80.30%) - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -9,7 +9,6 @@ class Solution:
             # create first two elements in dp array
             # first question, do we rob 1st house or second house?
             dp = [nums[0], max(nums[0], nums[1])]
-
             # if we rob 2nd house, next question is do we rob 1st + 3rd, or just second?
             for i in range(2, len(nums)):
                 dp.append(max(dp[i - 1], dp[i - 2] + nums[i]))
